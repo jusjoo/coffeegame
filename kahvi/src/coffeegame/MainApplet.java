fa8a38211045f84acbbea1f2f58ef025e3a05ba0
Joonas Jussila <Joonas Jussila@JoonasJussila>
2012-04-26 15:38:33 +0300

    vaihdettu compiler 1.6:ksi, koska applet hnggh!

---
+++ kahvi/src/coffeegame/MainApplet.java
@@ -0,0 +1,15 @@
+package coffeegame;
+
+
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplet;
+
+
+public class MainApplet extends LwjglApplet
+{
+    private static final long serialVersionUID = 1L;
+    public MainApplet()
+    {
+        super(new Main(), false);
+    }
+}
