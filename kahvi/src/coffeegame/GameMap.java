fd62fc5089e4e71960da1b5115648a8a92c14b33
GTA6 <GTA6@172.24.23.109>
2012-04-27 12:42:57 +0300

    lisätty pikkusen juttuja

---
+++ kahvi/src/coffeegame/GameMap.java
@@ -0,0 +1,19 @@
+package coffeegame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
+public class GameMap {
+
+	
+	public GameMap(FileHandle mapfile) {
+		
+	}
+	
+	
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- kahvi/src/coffeegame/Main.java
+++ kahvi/src/coffeegame/Main.java
@@ -3,14 +3,36 @@ package coffeegame;
 
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+
 
 public class Main implements ApplicationListener{
 
+	private SpriteBatch spriteBatch;
+	private Box2DDebugRenderer debugRenderer;
+	private Camera cam;
+	private GameMap currentMap;
+	
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
 		
+		spriteBatch = new SpriteBatch();
+		cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		
+		cam.position.x = Gdx.graphics.getWidth()/4;
+		cam.position.y = Gdx.graphics.getHeight()/4;
+		cam.update();
+		debugRenderer = new Box2DDebugRenderer();
+		
+		
+		spriteBatch.setProjectionMatrix(cam.combined);
 	}
 
 	@Override
@@ -27,7 +49,26 @@ public class Main implements ApplicationListener{
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		Gdx.graphics.setTitle(Integer.toString(Gdx.graphics.getFramesPerSecond()));
+		
+		Gdx.gl.glClearColor(0, 0, 0, 1.0f);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		cam.update();
+		
+		
+		// check if we have a map loaded and game is not paused
+		if (currentMap != null) {
+			update();
+		}
+		
+	}
+
+	private void update() {
+		
+		
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		
+		currentMap.update(deltaTime);
 		
 	}
 
@@ -40,6 +81,10 @@ public class Main implements ApplicationListener{
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public void loadMap() {
 		
 	}
 	
