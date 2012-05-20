31424c2ac967f39758184ffc0f5018edfdddaa69
Joonas Jussila <Joonas Jussila@JoonasJussila>
2012-05-20 15:54:01 +0300

    musiikkijuttuja

--- kahvi/src/dynamicmusic/MusicPlayer.java
+++ kahvi/src/dynamicmusic/MusicPlayer.java
@@ -6,18 +6,16 @@ import java.util.HashMap;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.utils.IdentityMap.Entry;
 
 import ec.Debug;
 
 public class MusicPlayer {
 
-	
-	private ArrayList<Sound> soundfiles;
-	
-	private Sound addSnare;
+	// all sound files with the excitement level they're triggered at
+	private HashMap<Sound, Integer> soundfiles;
 	
 	private int excitement;
-	private Sound addHats;
 	
 	private AssetManager manager;
 	
@@ -27,7 +25,7 @@ public class MusicPlayer {
 	
 	
 	public MusicPlayer() {
-		soundfiles = new ArrayList<Sound>();
+		soundfiles = new HashMap<Sound, Integer>();
 		playingSoundIDs = new HashMap<Sound, Long>();
 		
 		manager = new AssetManager();
@@ -47,11 +45,11 @@ public class MusicPlayer {
 	 */
 	public void play() {
 		if (isReady) {
-			for (Sound s : soundfiles) {
-				long id = s.play();
-				s.setVolume(id, 1);
+			for (Sound s : soundfiles.keySet()) {
+				long id = s.play(0f);
 				s.setLooping(id, true);
 				playingSoundIDs.put(s, id);
+				Debug.log("sound playing");
 			}
 		}
 	}
@@ -64,13 +62,14 @@ public class MusicPlayer {
 		playingSoundIDs.clear();
 		
 		// stop all sounds
-		for (Sound s : soundfiles) {
+		for (Sound s : soundfiles.keySet()) {
 			s.stop();
 		}
 	}
 	
 	public void setExcitement(int excitement) {
-
+		this.excitement = excitement;
+		Debug.log("Excitement level set to: " + excitement);
 	}
 	
 	public int getExcitementLevel() {
@@ -81,24 +80,84 @@ public class MusicPlayer {
 		manager.update();
 		if (!isReady) {
 			// check if all resources are ready
-			if (	   manager.isLoaded("assets/music/beat.ogg") 
-					&& manager.isLoaded("assets/music/addsnare.ogg") 
-					&& manager.isLoaded("assets/music/addhats.ogg") ) {
-				
-				soundfiles.add(manager.get("assets/music/beat.ogg", Sound.class));
-				addSnare = manager.get("assets/music/addsnare.ogg", Sound.class);
-				addHats = manager.get("assets/music/addhats.ogg", Sound.class);
-				soundfiles.add(addSnare);
-				soundfiles.add(addHats);
-				
-				isReady = true;
-				
-			} 
+			checkResources();
+			
 			
 		} else {
+			// else our assets are ready
 			
+			
+			// playing music?
+			if (!playingSoundIDs.isEmpty()) {
+				updateSoundVolumes();
+				
+				// excitement over 10, set pitch up one note
+				if (excitement >= 10) setPitch(1);
+			}
 
 		}
+	}
+
+
+	private void setPitch(int i) {
+		
+		float pitch = getPitch(i);
+		
+		for (Sound s :playingSoundIDs.keySet()) {
+			s.setPitch(playingSoundIDs.get(s), pitch);
+		}
+		
+	}
+
+
+
+	/*
+	 * Checks if all assets are loaded and ready to play
+	 * 
+	 * sets this.isReady = true when everything is loaded
+	 */
+	private void checkResources() {
+		
+		if (	   manager.isLoaded("assets/music/beat.ogg") 
+				&& manager.isLoaded("assets/music/addsnare.ogg") 
+				&& manager.isLoaded("assets/music/addhats.ogg") ) {
+			
+			soundfiles.put(manager.get("assets/music/beat.ogg", Sound.class), 0);
+			
+			soundfiles.put(manager.get("assets/music/addsnare.ogg", Sound.class), 3);
+		
+			soundfiles.put(manager.get("assets/music/addhats.ogg", Sound.class), 4);
+			
+			
+			isReady = true;
+			
+		} 
+	}
+
+
+
+	private void updateSoundVolumes() {
+		// go through all sounds
+		for (Sound s : soundfiles.keySet()) {
+			
+			// if global excitement level is higher than the sounds trigger level, we up the volume
+			if(excitement >= soundfiles.get(s)) {
+				// sound should be already playing, because we have synchronized play on all sounds
+				s.setVolume(playingSoundIDs.get(s), 1);
+			} 
+			// else we mute the sound
+			else {
+				s.setVolume(playingSoundIDs.get(s), 0);
+			}
+		}
+	}
+	
+	private float getPitch(int pitchInNotes) {
+		float pitch;
+		
+		pitch = (float) Math.pow(2.0, (pitchInNotes)/12.0);
+		Debug.log("Note Pitch: " + pitch);
+		return pitch;
 	}
 	
 }
